e421dfd7b20dff76b814a468dcc0d198189a8087
tabsheermk <creativemk12@gmail.com>
2026-02-28 21:21:08 +0530

    global config and mongoose connected

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await ConfigModule.forRoot({
+    isGlobal: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
